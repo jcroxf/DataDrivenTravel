2f861dd3bc21c5b7ab4d5fee417cf2bbbeda1ceb
Alec Riding <alec.riding@roq.co.uk>
2022-08-24 10:59:44 +0100

    Began to add in to the DBs

--- src/main/java/com/academy/travelapp/BaseDB.java
+++ src/main/java/com/academy/travelapp/BaseDB.java
@@ -1,21 +1,62 @@
 package com.academy.travelapp;
 
 import java.util.UUID;
-//import java.sql.ResultSet;
+import java.sql.ResultSet;
 
 
-public class BaseDB implements base {
+public class BaseDB implements base, Processor {
+	protected static final int MAX_NAME = 100;
+	protected static final int MAX_UUID = 36;
+	
+	protected UUID uuid;
+	protected String name;
 
 	@Override
 	public UUID getId() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.uuid;
 	}
 
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.name;
 	}
+	
+	public void process(ResultSet results) {
 
 }
+	protected BaseDB() {
+		this.uuid = UUID.randomUUID();
+		
+	}
+	protected void read(String query) {
+		try (TravelApp app = new TravelApp()) {
+			app.read(this, query);				
+		}
+	}
+	
+	protected void write(String query) {
+		try (TravelApp app = new TravelApp()) {
+			app.write(query);
+		}
+	}
+	
+	protected String validate(String value, int max) {
+		if ((value == null) || (value.length() < 1)) {
+			throw new RuntimeException("String is unspecified!");
+		}
+		
+		if (value.length() > max) {
+			throw new RuntimeException("String is too large!");
+		}
+		
+		if (!value.matches("^[a-zA-Z0-9 ]*$")) {
+			throw new RuntimeException("String can only be alphanumber with spaces!");
+		}
+		
+		return value;
+	}
+	
+}
+
--- src/main/java/com/academy/travelapp/CustomerDB.java
+++ src/main/java/com/academy/travelapp/CustomerDB.java
@@ -1,9 +1,14 @@
 package com.academy.travelapp;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class CustomerDB extends BaseDB implements Customer {
 
+	private static final String SQL_QUERY = "SELECT ticket.Price, ticket.TravelType, ticket.StationStart, ticket.StationEnd, ticket.CustomerId, ticket.TicketId FROM Ticket ticket, Customer customer WHERE customer.Id = '%s' AND ticket.CustomerId = customer.Id"; 
+	
 	@Override
 	public List<Ticket> getTickets() {
 		// TODO Auto-generated method stub
